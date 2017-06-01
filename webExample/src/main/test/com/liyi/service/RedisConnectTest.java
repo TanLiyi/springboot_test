53b7ce315fa8ba9badd35db5267763fd420825e1
TanLiyi <tan123456>
2017-06-01 18:49:14 +0800

    test commit

--- webExample/src/main/test/com/liyi/service/RedisConnectTest.java
+++ webExample/src/main/test/com/liyi/service/RedisConnectTest.java
@@ -1,5 +1,8 @@
 package com.liyi.service;
  
+import java.util.List;
+import java.util.Map;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +22,16 @@ public class RedisConnectTest {
 	@Test
 	public void test(){
 		// 保存字符串
-		stringRedisTemplate.opsForValue().set("aaa", "111");
-	/*	Assert.assertEquals("111", stringRedisTemplate.opsForValue().get("aaa"));*/
-		System.out.println(stringRedisTemplate.opsForValue().get("aaa"));
+		/*stringRedisTemplate.opsForValue().set("bfe", "111");*/
+//		stringRedisTemplate.opsForList().rightPush("list1", "1");
+//		stringRedisTemplate.opsForList().rightPush("list1", "2");
+		List<String> list=stringRedisTemplate.opsForList().range("list1" ,0, -1l);
+		System.out.println(list);
+//		list.stream().count();
+		System.out.println(list.stream().count());
+		
+		Map<Object,Object> map=stringRedisTemplate.opsForHash().entries("hash-key");
+		System.out.println(map);
     }
 
 }

f958ebf23df8ce29e2c2571b494be4d95d27360c
TanLiyi <tan123456>
2017-06-02 10:20:07 +0800

    去除没用的包和类

--- webExample/src/main/java/com/liyi/StartApplication.java
+++ webExample/src/main/java/com/liyi/StartApplication.java
@@ -4,17 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.data.jpa.repository.Query;
 
 @SpringBootApplication
-public class Application  extends SpringBootServletInitializer {
+public class StartApplication  extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(Application.class);
+		return application.sources(StartApplication.class);
 	}
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(StartApplication.class, args);
 	}
 }
--- webExample/src/main/java/com/liyi/service/PointService.java
+++
@@ -1,8 +0,0 @@
-package com.liyi.service;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public class PointService {
-
-}
